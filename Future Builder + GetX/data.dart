5f75bfef9133fba877606e7668d09907947c6f06
idekorslet <80518183+idekorslet@users.noreply.github.com>
2022-05-18 17:14:12 +0700

    update beberapa kode

--- Future Builder + GetX/data.dart
+++ Future Builder + GetX/data.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class Helper {
   List _allData = [];
-  bool loadLocalData = false;
+  bool downloadNewData = false;
   List<String> _categoryText = [];
   List _productByCategoryIndex = [];
   List<Map<String, dynamic>> _dataByCategory = [];
@@ -34,17 +34,18 @@ class Helper {
   }
 
   Future downloadData({required String apiUrl}) async {
-    List products = await loadData(apiUrl: apiUrl);
+    await loadData(apiUrl: apiUrl);
     _divideDataByCategory();
-    return products;
+    return allData;
   }
 
   void _divideDataByCategory() {
     String title = '';
     String category = '';
     String price = '';
+    String imageUrl = '';
     int index;
-    int currentIndex = -1;
+    int currentIndex = 0;
 
     _categoryText = [];
     _productByCategoryIndex = [];
@@ -53,17 +54,18 @@ class Helper {
       title = _allData[i]['title'];
       category = _allData[i]['category'];
       price = _allData[i]['price'].toString();
+      imageUrl = _allData[i]['image'];
       index = _categoryText.indexOf(category);
 
       // jika kategori tidak ditemukan maka input kategori baru ke list
       if (index < 0) {
-        currentIndex += _categoryText.length + 1;
+        currentIndex = _categoryText.length;
         _categoryText.add(category);
       } else {
         currentIndex = index;
       }
 
-      _productByCategoryIndex.add([currentIndex, [title, category, price]]);
+      _productByCategoryIndex.add([currentIndex, [title, category, price, imageUrl]]);
     }
 
     _categoryText.insert(0, 'All');
@@ -76,8 +78,9 @@ class Helper {
     int _curIndex = 0;
     _dataByCategory = [];
 
-    // looping sebanyak jumlah kategori
+    // looping sebanyak jumlah data yang kategorinya sudah diurutkan berdasarkan index kategori
     for (int i=0; i < _productByCategoryIndex.length; i++) {
+
       // jika nilai index data ke-n berbeda dengan nilai index sekarang
       if (_productByCategoryIndex[i][0] != _curIndex) {
         // simpan semua data ke list setelah semua data sesuai indexnya di kumpulkan
@@ -89,14 +92,17 @@ class Helper {
       _tempData[i.toString()] = {
         'title'   : _productByCategoryIndex[i][1][0],
         'category': _productByCategoryIndex[i][1][1],
-        'price'   : _productByCategoryIndex[i][1][2]
+        'price'   : _productByCategoryIndex[i][1][2],
+        'image'   : _productByCategoryIndex[i][1][3]
       };
     }
+
+    // masukkan data terakhir
+    _dataByCategory.add(_tempData);
   }
 
   List filterDataByCategory(int categoryIndex) {
     List result = [];
-    print(_dataByCategory[categoryIndex]);
     _dataByCategory[categoryIndex].forEach((key, value) {
       result.add(value);
     });
@@ -105,7 +111,7 @@ class Helper {
   }
 
   List<String> get categoryList => _categoryText;
-  List get allData => _productByCategoryIndex;
+  List get allData => _allData;
 }
 
 // void main() async {
