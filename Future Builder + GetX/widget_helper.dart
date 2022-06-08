604ddd40fac86ba3af79f46aaf92b3c3edbbdcb2
idekorslet <80518183+idekorslet@users.noreply.github.com>
2022-06-08 09:32:55 +0700

    Update widget_helper.dart

--- Future Builder + GetX/widget_helper.dart
+++ Future Builder + GetX/widget_helper.dart
@@ -5,8 +5,10 @@ class WidgetHelper {
   ButtonStyle btnCategoryStyle(bool changeColor) {
     return ButtonStyle(
         padding: MaterialStateProperty.all<EdgeInsets>(const EdgeInsets.all(10)),
-        foregroundColor: changeColor ? MaterialStateProperty.all<Color>(Colors.white) : MaterialStateProperty.all<Color>(Colors.blue),
-        backgroundColor: changeColor ? MaterialStateProperty.all<Color>(Colors.blue) : MaterialStateProperty.all<Color>(Colors.white),
+//         foregroundColor: changeColor ? MaterialStateProperty.all<Color>(Colors.white) : MaterialStateProperty.all<Color>(Colors.blue),
+//         backgroundColor: changeColor ? MaterialStateProperty.all<Color>(Colors.blue) : MaterialStateProperty.all<Color>(Colors.white),
+        foregroundColor: MaterialStateProperty.all<Color>(changeColor ? Colors.white : Colors.blue),
+        backgroundColor: MaterialStateProperty.all<Color>(changeColor ? Colors.blue : Colors.white),
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
             RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(18.0),
