49727294cfffd3a9d26eb6cf6e66bcaf0d244789
idekorslet <80518183+idekorslet@users.noreply.github.com>
2023-07-13 07:34:54 +0700

    Update routes.dart

--- Local Push Notification/routes.dart
+++ Local Push Notification/routes.dart
@@ -1,1 +1,38 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:local_push_notification/pages/home_page.dart';
+import 'package:local_push_notification/pages/notification_page.dart';
+import 'controllers/notification_controller.dart';
 
+class Routes {
+  static const String routeHome = '/';
+  static const String routeNotification = '/notification-page';
+
+  static List<Route<dynamic>> onGenerateInitialRoutes(String initialRouteName) {
+    List<Route<dynamic>> pageStack = [];
+    pageStack.add(MaterialPageRoute(builder: (_) => const MyHomePage()));
+
+    if (initialRouteName == routeNotification && NotificationController.initialAction != null) {
+      pageStack.add(MaterialPageRoute(
+          builder: (_) => NotificationPage(
+              receivedAction: NotificationController.initialAction!)
+          )
+      );
+    }
+    return pageStack;
+  }
+
+  static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case routeHome:
+        return MaterialPageRoute(builder: (_) => const MyHomePage());
+
+      case routeNotification:
+        ReceivedAction receivedAction = settings.arguments as ReceivedAction;
+        return MaterialPageRoute(
+            builder: (_) => NotificationPage(receivedAction: receivedAction)
+        );
+    }
+    return null;
+  }
+}
